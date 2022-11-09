70433d7cd0256a0c3daa8527dda3f096811fd469
Lewis Gaul <lewis.gaul@gmail.com>
2022-11-09 16:36:55 +0000

    Tidy up implementation of omino v1

--- src/main.zig
+++ src/main.zig
@@ -21,7 +21,9 @@ const Omino = struct {
     }
 
     pub fn toStr(self: Omino) ![]const u8 {
-        var buf: []u8 = try allocator.alloc(u8, 128);
+        // TODO: Should use 'std.fmt' or 'std.mem'?
+        var bufLen = self.size * (self.size + 1) - 1;
+        var buf: []u8 = try allocator.alloc(u8, bufLen);
         var i: u8 = 0;
         var j: u8 = 0;
         var index: u8 = 0;
@@ -35,30 +37,34 @@ const Omino = struct {
                 index += 1;
                 j += 1;
             }
-            buf[pos] = '\n';
-            pos += 1;
+            if (i < self.size - 1) { buf[pos] = '\n'; pos += 1; }
             i += 1;
         }
-        buf[pos] = 0;
-        pos += 1;
         
         return buf;
     }
 };
 
+/// Function to create a dummy omino.
+/// Returned omino should be deinitialised by the caller.
+fn createOmino() !Omino {
+    var omino = try Omino.init(4);
+    omino.array[2] = true;
+    omino.array[5] = true;
+    omino.array[6] = true;
+    omino.array[7] = true;
+    return omino;
+}
+
 /// Main.
 pub fn main() !void {
     std.debug.warn("Running...\n", .{});
     
-    var omino = try Omino.init(4);
+    var omino = try createOmino();
     defer omino.deinit();
-    omino.array[1] = true;
-    omino.array[4] = true;
-    omino.array[5] = true;
-    omino.array[15] = true;
     var omino_str = try omino.toStr();
     std.debug.warn("Omino:\n{s}\n", .{omino_str});
-    allocator.free(omino_str);
+    allocator.free(omino_str);  // Does this guarantee all the memory was freed?
     
     std.debug.warn("Finished\n", .{});
 }
